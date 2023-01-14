174d49f2784f7a49134087c6a0a02235fcb8739a
LinnickJera <linnick@jera.com.br>
2023-01-14 03:19:14 -0400

    [UPDATE] Added animated_text_kit in layout and finalized form buttons

--- lib/features/auth/sign_in_view.dart
+++ lib/features/auth/sign_in_view.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 import '../../localization/localize.dart';
@@ -15,61 +16,114 @@ class SignInView extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(44)),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 44,
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(44)),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CustomTextFormField(
-                    icon: Icons.email,
-                    label: l10n.emailInputLabel,
-                  ),
-                  CustomTextFormField(
-                    isSecret: true,
-                    icon: Icons.lock,
-                    label: l10n.passwordInputLabel,
-                  ),
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                /// Nome do App
+                const Text.rich(
+                  TextSpan(
+                    style: TextStyle(fontSize: 40),
+                    children: [
+                      TextSpan(
+                        text: 'Green',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      onPressed: () {},
-                      child: const Text(
-                        'Entrar',
-                        style: TextStyle(fontSize: 18),
+                      TextSpan(
+                        text: 'grocer',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
+                    ],
+                  ),
+                ),
+
+                /// Animação
+                SizedBox(
+                  height: 32,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(fontSize: 24),
+                    child: AnimatedTextKit(
+                      pause: Duration.zero,
+                      repeatForever: true,
+                      animatedTexts: [
+                        FadeAnimatedText('Frutas'),
+                        FadeAnimatedText('Verduras'),
+                        FadeAnimatedText('Legumes'),
+                        FadeAnimatedText('Carnes'),
+                        FadeAnimatedText('Cereais'),
+                        FadeAnimatedText('Laticínios'),
+                      ],
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Esqueceu sua senha?',
-                        style: TextStyle(color: Colors.green),
+                )
+              ],
+            ),
+          ),
+
+          /// Formulário
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 44,
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(44)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                /// Email
+                CustomTextFormField(
+                  icon: Icons.email,
+                  label: l10n.emailInputLabel,
+                ),
+
+                /// Senha
+                CustomTextFormField(
+                  isSecret: true,
+                  icon: Icons.lock,
+                  label: l10n.passwordInputLabel,
+                ),
+
+                /// Botão de entrar
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
                       ),
                     ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
-                  Row(
+                ),
+
+                /// Esqueceu sua senha
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Esqueceu sua senha?',
+                      style: TextStyle(color: Colors.green),
+                    ),
+                  ),
+                ),
+
+                /// Divisor
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: Row(
                     children: [
                       Expanded(
                         child: Divider(
@@ -89,15 +143,28 @@ class SignInView extends StatelessWidget {
                       ),
                     ],
                   ),
-                  SizedBox(
-                    height: 50,
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: const Text('Criar conta'),
+                ),
+
+                /// Botão de Criar conta
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Colors.green,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Criar conta',
+                      style: TextStyle(fontSize: 16),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           )
         ],
